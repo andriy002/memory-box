68a2536cb060aae46cb502d442164f9d9f6b28a1
andriy002 <you@example.com>
2022-01-16 16:07:32 +0200

    bug fix

--- lib/pages/main_pages/main_page/main_page.dart
+++ lib/pages/main_pages/main_page/main_page.dart
@@ -20,8 +20,12 @@ class MainPage extends StatelessWidget {
   static Widget create() {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ViewModelAudio()),
-        ChangeNotifierProvider(create: (_) => ViewModelAudioPlayer()),
+        ChangeNotifierProxyProvider(
+            create: (_) => ViewModelAudio(),
+            update: (_, ___, __) => ViewModelAudio()),
+        ChangeNotifierProxyProvider(
+            create: (_) => ViewModelAudioPlayer(),
+            update: (_, ___, __) => ViewModelAudioPlayer()),
         StreamProvider(
           create: (_) => CollectionsRepositories.instance.colllections,
           initialData: null,
